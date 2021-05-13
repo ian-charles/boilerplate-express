c96ba490ffb190b5f1b0dba8c45ec091c33e738a
Ian Charles <iancharles11@gmail.com>
2021-05-13 13:17:52 -0700

    Hello Express

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+const port = 3000;
 console.log("Hello World!")
 
+app.get('/', (req, res) => {
+	res.send('Hello Express')
+})
 
+app.listen(port, () => {
+	console.log(`The app is listening at http://localhost:${port}`)
+})
 
 
 
